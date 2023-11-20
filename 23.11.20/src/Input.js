c451ed01a3de2aa777873188cba5e1b92993b94d
viktorija <viktorija.kotaite@gmail.com>
2023-11-20 18:25:17 +0200

    autocomplete

--- 23.11.20/src/Input.js
+++ 23.11.20/src/Input.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 
 export default function Input() {
   const [value, setValue] = useState();
+  const [selected, setSelected] = useState()
   const names = ["Marija", "Andrius", "Tomas", "Viktorija", "Justina", "Laima", "Arvydas","Elena", "Dominyka", "Giedrius", "Justinas"];
-
+ 
   const handleChange = (e) => {
     // e.preventDefault()
-
     return setValue(
       e.target.value
         ? names.filter((name) =>
@@ -15,8 +15,12 @@ export default function Input() {
           )
         : ""
     );
-
   };
+
+  const selectEl = (e) => {
+     return setSelected(e.target.text) 
+   }
+
   return (
     <>
       <div className="container my-5">
@@ -26,13 +30,14 @@ export default function Input() {
           name="tekstas"
           placeholder="Įveskite vardą"
           onKeyUp={handleChange}
+          value={selected}
         />
 
         <ul>
           {value
             ? value.map((el, i) => (
                 <li key={i}>
-                  <a href="#">{el}</a>
+                  <a href="#" onClick={selectEl}>{el}</a>
                 </li>
               ))
             : value}
